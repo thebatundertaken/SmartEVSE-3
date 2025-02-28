8ccd2ebefd07f8d65ac6b646be4822ad953e2b8e
Serg1o <scuellarfer@gmail.com>
2025-02-28 15:19:45 +0100

    Bug fix: device menu Sensorbox submenu options

--- SmartEVSE-3-scf/src/i18n.cpp
+++ SmartEVSE-3-scf/src/i18n.cpp
@@ -24,35 +24,35 @@
 #include <stdlib.h>
 
 const char* geti18nStrMeterText(uint8_t statusCode) {
-  if (statusCode < 9) {
-    return i18nStrMeter[statusCode];
-  }
+    if (statusCode < 10) {
+        return i18nStrMeter[statusCode];
+    }
 
-  return "Unknown";
+    return "Unknown";
 }
 
 const char* geti18nRfidStatusText(uint8_t code) {
-  if (code < 8) {
-    return i18nStrRFIDStatus[code];
-  }
+    if (code < 8) {
+        return i18nStrRFIDStatus[code];
+    }
 
-  return "Unknown";
+    return "Unknown";
 }
 
 const char* geti18nStateText(uint8_t stateCode) {
-  if (stateCode < 11) {
-    return i18nStrStateNameWeb[stateCode];
-  }
+    if (stateCode < 11) {
+        return i18nStrStateNameWeb[stateCode];
+    }
 
-  return "Unknown";
+    return "Unknown";
 }
 
 const char* geti18nErrorText(uint8_t ErrorCode) {
-  uint8_t count = 0;
-  // find the error bit that is set
-  while (ErrorCode) {
-    count++;
-    ErrorCode = ErrorCode >> 1;
-  }
-  return (count < 9) ? i18nStrErrorNameWeb[count] : "Multiple Errors";
+    uint8_t count = 0;
+    // find the error bit that is set
+    while (ErrorCode) {
+        count++;
+        ErrorCode = ErrorCode >> 1;
+    }
+    return (count < 9) ? i18nStrErrorNameWeb[count] : "Multiple Errors";
 }
